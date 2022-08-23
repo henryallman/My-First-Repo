fa38c3975fd2f670c0c22ebd524f094b90b73bc2
henryallman <hnallman@stu.naperville203.org>
2022-08-23 15:07:51 -0500

    learned bluej and took notes

---
+++ My First BlueJ Project/Akhil.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class Akhil here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Akhil
+{
+    
+    /**
+     * Constructor for objects of class Akhil
+     */
+    public Akhil()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *      
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public static void main(String[] args)
+    {
+        //basic data type
+        double number = 8.9,number2;
+        int x = 9,y,z;
+        boolean choice = false;
+        char letter = 'A';
+        long n2 = 3;
+        short n3 = 7;
+        
+        
+        //these are classes and objects
+        //Class      ID         memory   Constructor
+        //Name                
+        Integer      n1 =       new      Integer(47);
+        String       word =     new      String("Akhil Kanuri");
+        Double       number4 =  new      Double(6.9);
+        
+        System.out.print(word.length());
+        
+        
+        
+    }
+}
